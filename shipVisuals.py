095d51dd7270dcbad1fd670b92a6e19738d66e76
Shrey Kothari <shrey.gk1@gmail.com>
2025-11-22 10:00:53 -0800

    created both functions

---
+++ shipVisuals.py
@@ -0,0 +1,9 @@
+import matplotlib as plt
+import numpy as np
+import pandas as pd
+
+def originalManifest(manifest):    #visualize the original manifest
+    return 0
+
+def containersVisualization(newManifest):   #visualize each stage of the process
+    return 0
