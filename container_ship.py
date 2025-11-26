4001497d06f5a3066b378a6b3135b2c70fde16e8
bryxnf <bfran029@ucr.edu>
2025-11-25 19:08:51 -0800

    implementing containership which contains the ship data, moving, sliding, balance checks, and goal detection ... still in progress

--- container_ship.py
+++ container_ship.py
@@ -1,5 +1,6 @@
 from collections import namedtuple
 from typing import List, Tuple, Optional
+from manifestParser import ManifestParser
 
 MAX_ROWS = 8
 MAX_COLS = 12
@@ -8,4 +9,10 @@ MAX_COLS = 12
 ContainerMove = namedtuple('ContainerMove',['start_pos','end_pos','container_weight','cost'])
 
 class ContainerShip:
-    
+    def __init__(self, manifest_file: str): 
+        #initialize the ship with the manifest file
+        parser = ManifestParser()
+        manifest_entries = parser.parse_manifest(manifest_file)
+
+        #converts data to (row,col,weight) tuples
+        manifest_data = [(pos[0],pos[1],weight) for pos, weight, desc in manifest_entries]
