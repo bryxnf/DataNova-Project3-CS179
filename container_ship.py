d9f54ebab8eb6e96cda03680d4fe71b4a50a34f8
bryxnf <bfran029@ucr.edu>
2025-11-27 12:33:15 -0800

    defining a couple of functions

--- container_ship.py
+++ container_ship.py
@@ -16,3 +16,29 @@ class ContainerShip:
 
         #converts data to (row,col,weight) tuples
         manifest_data = [(pos[0],pos[1],weight) for pos, weight, desc in manifest_entries]
+
+
+        #initialize grid/weights
+        self.grid = [[0 for _ in range(MAX_COLS)] for _ in range(MAX_ROWS)]
+        self.total_weight = 0
+        self.port_weight = 0
+        self.max_row = MAX_ROWS
+        self.max_col = MAX_COLS
+
+        #to produce the grid
+        self.parse_manifest(manifest_data)
+
+    def grid_tuple(self):
+        return tuple(tuple(row) for row in self.grid())
+    
+    def get_top_container(self,col: int)-> Tuple[Optional[Tuple[int, int]], int]:
+        for r in range(MAX_ROWS - 1, -1, -1):
+            if self.grid[r][col] != 0:
+                return (r+1, col + 1), self.grid[r][col]
+        return None, 0
+        
+    def get_next_empty(self,col: int)-> Optional[Tuple[int, int]]:
+        for r in range(MAX_ROWS):
+            if self.grid[r][col] == 0:
+                return (r+1, col + 1)
+        return None
