9f87836cbd8cce3bf9224b1bbb40abbfed8199d5
bryxnf <bfran029@ucr.edu>
2025-11-27 16:52:17 -0800

    more functions

--- container_ship.py
+++ container_ship.py
@@ -28,17 +28,56 @@ class ContainerShip:
         #to produce the grid
         self.parse_manifest(manifest_data)
 
+    #created this function to make the tuple of tuples unchangleable(so i can compare the ship for search algorithms)
     def grid_tuple(self):
         return tuple(tuple(row) for row in self.grid())
     
+    #finds the top container within the column
     def get_top_container(self,col: int)-> Tuple[Optional[Tuple[int, int]], int]:
         for r in range(MAX_ROWS - 1, -1, -1):
             if self.grid[r][col] != 0:
                 return (r+1, col + 1), self.grid[r][col]
         return None, 0
-        
+
+    #finds the first empty slot from the bottom up in a column  
     def get_next_empty(self,col: int)-> Optional[Tuple[int, int]]:
         for r in range(MAX_ROWS):
             if self.grid[r][col] == 0:
                 return (r+1, col + 1)
         return None
+    
+    #places the data into each grid within the ship
+    def parse_manifest(self, manifest_data: List[Tuple[int, int, int]]):
+        for r, c, w in manifest_data:
+            r0 = r -1
+            c0 = c -1
+            if 0 <= r0 < MAX_ROWS and 0 <= c0 < MAX_COLS:
+                if self.grid[r0][c0] != 0:
+                    print(f"Warning: overriding container at ({r}, {c})")
+                self.grid[r0][c0] = w
+                self.total_weight += w
+                if c0 < (MAX_COLS // 2):
+                    self.port_weight +=w
+                else:
+                    self.starboard_weight += w
+            else: 
+                print(f"Warning: invalid position ({r},{c}) ignored.")
+
+    #to check if there is nothing above the cur container(will be used for horizontal sliding)
+    def if_exposed(self,row_idx: int, col_idx: int)-> bool:
+        if self.grid[row_idx][col_idx]:
+            return False
+        for rr in range(row_idx + 1, MAX_ROWS):
+            if self.grid[rr][col_idx] != 0:
+                return False
+        return True
+    
+    #to check whether its the first container or has a container below it
+    def is_supported(self,row_idx: int, col_idx: int) -> bool:
+        if row_idx == 0:
+            return True
+        return self.grid[row_idx - 1][col_idx] != 0 
+    
+    
+    
+        
